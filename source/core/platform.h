cc9a1afac9ae14f40a15fd73027dfa7d45e6bb19
Eric Fedosejevs <eric.fedosejevs@gmail.com>
2021-03-24 19:57:07 -0500

    CMake update

--- source/core/platform.h
+++ source/core/platform.h
@@ -7,21 +7,8 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
-constexpr int _ce_strcmp_internal(const char* a, const char* b)
-{
-	return *a == 0 && *b == 0 ? 0 :
-		*a == 0 ? -1 :
-		*b == 0 ? 1 :
-		*a < *b ? -1 :
-		*a > *b ? 1 :
-		*a == *b ? _ce_strcmp_internal(a + 1, b + 1) :
-		throw;
-}
-
-constexpr int ce_strcmp(const char* a, const char* b)
-{
-	return (_ce_strcmp_internal(a, b) == 0);
-}
+constexpr int _ce_strcmp_internal(const char* a, const char* b);
+constexpr int ce_strcmp(const char* a, const char* b);
 
 // OS flags
 // Linux and Linux - derived			__linux__
